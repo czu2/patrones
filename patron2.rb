fdfdcb6522a081a8b1e1a1913341f90f103c3525
Cristian Zúñiga <cz.cristian@gmail.com>
2021-01-12 02:34:52 -0300

    Desafío Patrones

---
+++ patron2.rb
@@ -0,0 +1,28 @@
+num = ARGV[0].to_i
+
+def patron(num_pat)
+    pat = ''.to_s
+    cont =1
+
+    while cont <= num_pat
+        if cont <= num_pat
+            pat = pat + '*'
+            cont += 1
+        end
+        if cont <= num_pat
+            pat = pat + '*'
+            cont += 1
+        end
+        if cont <= num_pat
+            pat = pat + '.'
+            cont += 1
+        end
+        if cont <= num_pat
+            pat = pat + '.'
+            cont += 1
+        end
+    end
+    return pat
+end
+
+puts patron(num)
---
+++ patron4.rb
@@ -0,0 +1,25 @@
+num = ARGV[0].to_i
+
+def patron(num_pat)
+    pat = ''.to_s
+    cont =1
+
+    while cont <= num_pat
+        if cont <= num_pat
+            pat = pat + '1'
+            cont += 1
+        end
+        if cont <= num_pat
+            pat = pat + '2'
+            cont += 1
+        end
+        if cont <= num_pat
+            pat = pat + '3'
+            cont += 1
+        end
+    end
+    return pat
+end
+
+puts patron(num)
+
